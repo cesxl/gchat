4b0f9a4056f641b1bd0adecbd1cd669c81df309c
gc <liyi_hfuu@yeah.com>
2020-04-12 12:43:29 +0800

    update config

--- core/src/main/java/com/demo/common/utils/EncodeUtil.java
+++ core/src/main/java/com/demo/common/utils/EncodeUtil.java
@@ -2,8 +2,6 @@ package com.demo.common.utils;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
-import org.apache.shiro.crypto.hash.Hash;
-import org.apache.shiro.crypto.hash.SimpleHash;
 
 public class EncodeUtil {
 
@@ -25,11 +23,11 @@ public class EncodeUtil {
         return new String(Hex.encodeHex(input));
     }
 
-    public static void main(String[] args) {
-        String plain = "0";
-        byte[] salt = EncodeUtil.decodeHex("d2c6f64a");
-        Hash hash = new SimpleHash("SHA1", "0", salt, 1024);
-        byte[] hashPassword = hash.getBytes();
-        System.out.println(EncodeUtil.encodeHex(hashPassword));
-    }
+//    public static void main(String[] args) {
+//        String plain = "cesgroup";
+//        byte[] salt = EncodeUtil.decodeHex("d2c6f64a");
+//        Hash hash = new SimpleHash("SHA1", plain, salt, 1024);
+//        byte[] hashPassword = hash.getBytes();
+//        System.out.println(EncodeUtil.encodeHex(hashPassword));
+//    }
 }
--- web/src/main/java/com/demo/modules/ws/WebSocketConfig.java
+++ web/src/main/java/com/demo/modules/ws/WebSocketConfig.java
@@ -7,6 +7,12 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 @Configuration
 public class WebSocketConfig {
 
+    /**
+     * (使用内嵌容器启动时需要，如 WAR 包部署独立容器，则需去除)
+     * <p>
+     * 注入ServerEndpointExporter，
+     * 这个bean会自动注册使用了@ServerEndpoint注解声明的Websocket endpoint
+     */
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
